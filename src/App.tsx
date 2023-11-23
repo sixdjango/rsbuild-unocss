69cf2dc88c8f162781dcfd12e4f74505c8917e6e
django <six.django@gmail.com>
2023-11-23 10:05:52 +0800

    upgrade rsbuild

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import './App.css';
 
 const App = () => {
   return (
-    <div className="content px-2 flex items-center">
-      <h1>Rsbuild with React haha</h1>
+    <div className="content px-2 flex items-center justify-between">
+      <h1>Rsbuild with React</h1>
       <p>Start building amazing things with Rsbuild.</p>
     </div>
   );
