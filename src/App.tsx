a5af3557094c6680001cd2518889a2b28bc55c31
django <six.django@gmail.com>
2023-11-23 09:59:16 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="content px-2 flex items-center">
+      <h1>Rsbuild with React haha</h1>
+      <p>Start building amazing things with Rsbuild.</p>
+    </div>
+  );
+};
+
+export default App;
